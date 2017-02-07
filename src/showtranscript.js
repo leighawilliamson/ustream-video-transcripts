2118853ae2cb7fdae611cf87b43e1c0c3c5b30ed
leighwilliamson <leighw@us.ibm.com>
2017-02-07 00:46:20 -0600

    style work on show transcript page

--- src/showtranscript.js
+++ src/showtranscript.js
@@ -5,7 +5,7 @@
 function createTableRow(sentence) {
   //var dateObj = new Date(date);
   //var formattedDate = dateObj.toLocaleDateString() + " " + dateObj.toLocaleTimeString();
-  return '<tr> <td>' + sentence + '</td></tr>';
+  return '<tr><td contenteditable="false"><button type="submit" disabled>Update</button></td>  <td style="padding:4px" contenteditable="true">' + sentence + '</td></tr>';
 }
 
 /**
@@ -16,11 +16,11 @@ var sentences = [];
 function populateTable() {	
   var table = $("#transcript_table tr");
   $.get("/view_transcript", function (data) {
-      console.log('data: ', data);
+    //  console.log('data: ', data);
     sentences = JSON.parse(data);
-      console.log('sentences: ', sentences);
+    //  console.log('sentences: ', sentences);
     sentences.forEach(function (sentence) {
-        console.log('sentence: ', sentence);
+    //    console.log('sentence: ', sentence);
         var html = createTableRow(sentence);
         table.last().before(html);		
      });
